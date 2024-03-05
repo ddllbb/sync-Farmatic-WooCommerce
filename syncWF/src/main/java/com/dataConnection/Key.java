71a31390607e6893177853af343e3de3104e6c42
david leal <101174030+ddllbb@users.noreply.github.com>
2024-03-05 09:35:40 +0100

    Update Key.java

--- syncWF/src/main/java/com/dataConnection/Key.java
+++ syncWF/src/main/java/com/dataConnection/Key.java
@@ -3,8 +3,8 @@ package com.dataConnection;
 public class Key {
 		
 		//WooCommerce FuenteCarrantona
-		public static final String CONSUMER_KEY_FUENTEC = "ck_d6445233c3510c084209e0347549999285d3eb4a"; 
-		public static final String CONSUMER_SECRET_FUENTEC= "cs_8ba2e34d5426adab00e357467140c1d2f6fd8501";
+		public static final String CONSUMER_KEY_FUENTEC = ""; 
+		public static final String CONSUMER_SECRET_FUENTEC= "";
 		// WooComerce Miro, 57
 		public static final String CONSUMER_KEY_MIRO = ""; 
 		public static final String CONSUMER_SECRET_MIRO = "";
